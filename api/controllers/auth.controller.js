53e530c8ccdd558948156285acc89f1a82af9d1f
Ashish <theashish752@gmail.com>
2023-12-07 21:27:43 +0530

    create a middleware and a funtion to handle the possible manula error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req,res)=>{
+export const signup = async(req,res, next)=>{
     // console.log(req.body);  // to show in the terminal 
     const {username, email, password} = req.body;  
     const hashedPassword = bcryptjs.hashSync(password, 10) ;
@@ -10,6 +11,8 @@ export const signup = async(req,res)=>{
         await newUser.save();
         res.status(201).json('user created successfully');
     } catch(error){
-        res.status(404).json(error.message);
+        // res.status(404).json(error.message);
+        next(error);
+        // next(errorHandler(550,'error from the funtion'));
     }
 };
