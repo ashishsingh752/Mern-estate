27cba9c0a57e3787da3f1bdb9ea69a30a0964699
Ashish <theashish752@gmail.com>
2023-12-09 07:57:16 +0530

    complete sign in functionallity

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const Navigate= useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      Navigate('/')
+      
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>
-      SignIn
+    <div className="p-3 max-w-lg center mx-auto">
+      <h1 className="text-3xl my-7 text-center font-semibold"> sign in</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        <input
+          type="email"
+          placeholder="Email"
+          className="border rounded-lg p-3"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border rounded-lg p-3"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="uppercase bg-slate-700 p-3 text-white rounded-lg hover:opacity-95 disabled:opacity-80">
+          {loading? 'Loading':'Sign In'}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700 ">sign-up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
+  );
 }
